664faf4db5cc2a0e571ad8434c7aaf6e44aa97cd
lucviery <luciano.gonzaga@buson.com.br>
2023-02-12 12:40:30 -0300

    ajustes

--- src/views/cabine/ClassificationEquipCopa.vue
+++ src/views/cabine/ClassificationEquipCopa.vue
@@ -1,23 +1,40 @@
 <template>
       <div class="frame">
-        <div name="cabecalho" class="cabecalho">
+        <div name="cabecalho" class="titulo">
             Resultado Copa F1AV
         </div>
+
         <div
-            class="position_master"
-            v-for="equip of tableEquip"
-            :key="equip.nameEquip"
+          class="position_master"
+          v-for="equip of tableEquip"
+          :key="equip.nameEquip"
         >
-        <div>
-          {{ equip.position }}
+          <div class="position_equipe_logo table-cell">
+            <div :class="getClassColorPosition(equip.position)">
+              {{ equip.position }}
+            </div>
+            <div class="coll_div_barra">
+              <img
+                class="image_cabecalho"
+                :src="getBarraEquip(equip.equip)"
+                alt="Equipes"
+              />
+            </div>
+            <div class="coll_div_barra coll_texto_equipe">
+              {{ equip.nameEquip }}
+            </div>             
+          </div>         
+          <div class="position_pts table-cell">
+            <div class="number_pts">{{ equip.totalPts }}</div>
+          </div>
         </div>
-        <div>
-          {{ equip.nameEquip }}
-        </div>
-        <div>
-          {{ equip.totalPts }}
-        </div>       
-        </div>
+        <div name="rodape" class="rodape">
+          <img
+            class="image_cabecalho"
+            src="../../assets/endereco_site.png"
+            alt="Equipes"
+          />
+        </div>        
       </div>
 </template>
 <script>
@@ -66,10 +83,9 @@ export default {
 
     if (!this.isError) {
         if (
-          this.$route.query.data !== null &&
-        !isNaN(this.$route.query.data)
+          this.$route.query.data !== null
         ) {
-            this.dataEvent = datathis.$route.query.data;
+            this.dataEvent = this.$route.query.data;
 
             TableDrivers.getTableEquipCopaComData(this.$route.query.categoria, this.$route.query.temporada, this.dataEvent)
             .then((result) => {
@@ -89,5 +105,42 @@ export default {
         }
     }
   },
+  methods: {
+    getClassColorPosition(position) {
+      if (position <= 1) return "coll_div_position destaque_position";
+      else if (position >= 2) return "coll_div_position descenso_position";
+      else return "coll_div_position meio_position";
+    },
+    getBarraEquip(equip) {
+      if (equip.trim() !== "") {
+        let pic = equip.replace(/\s+/g, "");
+
+        var images = require.context(
+          "../../assets/barras-equipes/",
+          false,
+          /\.png$/
+        );
+
+        return images("./" + pic + ".png");
+      } else {
+        return "";
+      }
+    },    
+  },
 };
 </script>
+
+<style>
+  .titulo {
+    text-align: center;
+    padding-left: 5px;
+    padding-top: 17px;
+    padding-bottom: 17px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 25px;
+    line-height: 22px;
+    color: white;
+  }
+</style>
